5cb7d07612d6b06a69ab2ad0496459298333a705
heymj317 <heymj317@users.noreply.github.com>
2022-08-03 18:44:17 -0500

    added client-side processing and to build link tree

--- static/app.js
+++ static/app.js
@@ -13,8 +13,33 @@ const $userform = $("#user-form");
 //     //SEARCH DATABASE, POPULATE RESULTS TO WEBPAGE
 // });
 //FILE TREE STRUCTURE
+
+
 $(function () {
-    $('#jstree_demo_div').jstree();
+    $.jstree.defaults.core.data = true;
+    $.jstree.defaults.core.check_callback = true;
+
+
+    $('#jstree_results').jstree({
+        'core': {
+            'data': [
+                'Simple root node',
+                {
+                    'text': 'Root node 2',
+                    'state': {
+                        'opened': true,
+                        'selected': true
+                    },
+                    'children': [
+                        { 'text': 'Child 1' },
+                        'Child 2'
+                    ]
+                }
+            ]
+        }
+    });
+
+
     console.log("jstree is RUNNING");
     $('button').on('click', function () {
         $('#jstree_demo_div').jstree(true).select_node('child_node_1');
@@ -22,54 +47,139 @@ $(function () {
         $.jstree.reference('#jstree_demo_div').select_node('child_node_1');
     });
 
-    $('#jstree_demo_div').on("changed.jstree", function (e, data) {
-        console.log(data.node.text);
-    });
+    $('form').on('submit', function (event) {
+        $('#jstree_demo_div').jstree({
+            'core': {
+                'data': function () {
+                    let nodes = [];
+                    event.preventDefault();
+                    const userInput = $input.val();
+                    const searchTerm = encodeURIComponent(`${userInput}`);
 
-});
+                    if (isValidUrl(userInput)) {
+                        $.get(`/query/${searchTerm}`, (data) => {
+                            const result = JSON.stringify(data);
+                            let $results = $("#jstree_results").empty();
 
+                            let $row = $(`<div class="row"></div>`);
+                            $row.html(`<div class="row">${result}</div>`);
+                            $row.appendTo($results);
 
-
-$userform.submit(function (event) {
-    event.preventDefault();
-    const userInput = $input.val();
-    const searchTerm = encodeURIComponent(`${userInput}`);
-    $.get(`/query/${searchTerm}`, (data) => {
-        const result = JSON.stringify(data);
-        let $results = $("#results").empty();
-
-        let $row = $(`<div class="row"></div>`);
-        $row.html(`<div class="row">${result}</div>`);
-        $row.appendTo($results);
-
-        return data;
+                            return data;
 
 
-    }).then(data => {
-        let paths = data;
-        let result = [];
-        let level = { result };
+                        }).then(data => {
+                            let paths = data;
+                            let result = [];
+                            let level = { result };
 
-        for (var i = 0; i < data.length; i++) {
-            const url = data[i]['url'];
-            url.split('/').reduce((r, name, i, a) => {
-                if (!r[name]) {
-                    r[name] = { result: [] };
-                    r.result.push({ text: name, children: r[name].result })
+                            for (var i = 0; i < data.length; i++) {
+                                const url = data[i]['url'];
+                                url.split('/').reduce((r, name, i, a) => {
+                                    if (!r[name]) {
+                                        r[name] = { result: [] };
+                                        r.result.push({ text: name, children: r[name].result })
+                                    }
+
+                                    return r[name];
+                                }, level)
+                            }
+
+                            console.log(result)
+                            nodes = result;
+                            return data;
+
+                        });
+                    };
+                    console.log(nodes);
+                    return nodes;
                 }
-
-                return r[name];
-            }, level)
-        }
-        paths.forEach(path => {
-
-        })
-
-        console.log(result)
+            }
+        });
     });
+
+    $('#jstree_results').on("changed.jstree", function (e, data) {
+        console.log(data.node);
+    });
+
+    $userform.submit(function (event) {
+        event.preventDefault();
+        const userInput = $input.val();
+        const searchTerm = encodeURIComponent(`${userInput}`);
+
+        if (isValidUrl(userInput)) {
+            $.get(`/query/${searchTerm}`, (data) => {
+                const result = JSON.stringify(data);
+                let $results = $("#jstree_results").empty();
+
+                let $row = $(`<div class="row"></div>`);
+                $row.html(`<div class="row">${result}</div>`);
+                $row.appendTo($results);
+
+                return data;
+
+
+            }).then(data => {
+                let paths = data;
+                let result = [];
+                let level = { result };
+
+                for (var i = 0; i < data.length; i++) {
+                    const url = data[i]['url'];
+                    url.split('/').reduce((r, name, i, a) => {
+                        if (!r[name]) {
+                            r[name] = { result: [] };
+                            r.result.push({ text: name, children: r[name].result })
+                        }
+
+                        return r[name];
+                    }, level)
+                }
+                paths.forEach(path => {
+
+                })
+
+                console.log(result)
+
+
+                $('div:jstree_results').jstree(true).destroy();
+                $('#jstree_results').jstree(true).destroy();
+                $('#jstree_demo_div').jstree();
+                $('#jstree_demo_div').jstree({
+                    'core': {
+                        'data': result
+                    }
+                });
+
+                const tree = JASON.stringify(result);
+                $('#jstree_demo_div').jstree({
+                    'core': {
+                        'data': `${tree}`
+                    }
+                });
+                return data;
+
+                //const tree = JSON.stringify(data);
+                let $tree = $("#jstree_results").empty();
+
+
+
+            });
+        };
+
+
+    });
+
 });
 
 
+
+
+const isValidUrl = urlString => {
+    var a = document.createElement('a');
+    a.href = urlString;
+    return (a.host && a.host != window.location.host);
+}
 
 
 
